e9a06807b4daa617d8d7d9db81b8b3fb7fa549f7
Maged A. Saad <magedsaadaziz@hotmail.com>
2018-09-11 00:14:27 +0200

    wsgi update # 5

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-activate_this = '/path/to/env/bin/activate_this.py'
+activate_this = '/var/www/FLASKAPPS/CatalogAPP/venv/bin/activate_this.py'
 execfile(activate_this, dict(__file__=activate_this))
 import sys
 from pages import app as application
