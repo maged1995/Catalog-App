b38881afb72a11ede73b87f58263f8616dbb6dc1
Maged A. Saad <magedsaadaziz@hotmail.com>
2018-09-11 13:31:53 +0200

    added temporary lines

--- CatalogAPP/pages.py
+++ CatalogAPP/pages.py
@@ -346,3 +346,8 @@ def gdisconnect():
                                             'for given user.', 400))
         response.headers['Content-Type'] = 'application/json'
         return response
+
+if __name__ == '__main__':
+    app.secret_key = 'super_secret_key'
+    app.debug = True
+    app.run(host='0.0.0.0', port=5000)
